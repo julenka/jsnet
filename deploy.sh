73d10b14fbc94a7f78b81a50d5f2ddbee98e6ae3
julenka <julenka@cs.cmu.edu>
2014-07-27 23:55:16 -0400

    Adding release config file, updating deploy script to have staging and release modes

--- deploy.sh
+++ deploy.sh
@@ -7,12 +7,27 @@ set -e
 # http://unix.stackexchange.com/questions/12068/how-to-measure-time-of-program-execution-and-store-that-inside-a-variable
 START=$(date +%s.%N)
 
-echo 'building site...'
-jekyll build --config _config_staging.yml
 
 # scp -r  _site/* notjulie@notjulie.com:public_html/js_net
-echo 'deploying site...'
-rsync -rav -e ssh _site/* notjulie@notjulie.com:public_html/js_net
+
+if [[ "${1}" == "staging" ]] 
+    then
+    echo 'building staging...'
+    jekyll build --config _config_staging.yml
+
+    echo 'deploying staging...'
+    rsync -rav -e ssh _site/* notjulie@notjulie.com:public_html/js_net
+elif [[ "${1}" == "release" ]]
+    then
+    echo 'building release...'
+    jekyll build --config _config_release.yml
+
+    echo 'deploying release...'
+    rsync -rav -e ssh _site/* notjulie@notjulie.com:public_html/notjulie
+else
+    echo "${0} [staging|release]"
+    exit 1
+fi
 
 END=$(date +%s.%N)
 DIFF=$(echo "$END - $START" | bc)
