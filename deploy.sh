3f1adc0d6e849c3d098f41f99a109a5ecbe6300c
julenka <julenka@cs.cmu.edu>
2014-07-27 22:17:07 -0400

    New deploy script, adding local and staging configs, making lots of tweaks to make sure the top banner looks *perfect*

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
-scp -r  _site/* notjulie@notjulie.com:public_html/js_net
+
+# terminate in case of failur
+# http://stackoverflow.com/questions/4381618/exit-a-script-on-error
+set -e 
+
+# http://unix.stackexchange.com/questions/12068/how-to-measure-time-of-program-execution-and-store-that-inside-a-variable
+START=$(date +%s.%N)
+
+echo 'building site...'
+jekyll build --config _config_staging.yml
+
+# scp -r  _site/* notjulie@notjulie.com:public_html/js_net
+echo 'deploying site...'
+rsync -rav -e ssh _site/* notjulie@notjulie.com:public_html/js_net
+
+END=$(date +%s.%N)
+DIFF=$(echo "$END - $START" | bc)
+
+echo "deploy took ${DIFF} seconds"
